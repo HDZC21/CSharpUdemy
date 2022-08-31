99750ddaa90f779de82ca1beb4bba64c19373392
hdzc21 <hadzic.nenin@gmail.com>
2022-08-31 21:08:26 +0930

    Add Section 2 Answers

--- Intermediate/IntermediateExcersises/IntermediateExcersises/Main.cs
+++ Intermediate/IntermediateExcersises/IntermediateExcersises/Main.cs
@@ -6,8 +6,8 @@
     {
       // Section 2 Soultions
       Section2 sectionTwo = new Section2();
-      sectionTwo.RunStopwatch();
-      //Section2.ProblemTwo();
+      //sectionTwo.ExcersiseOne();
+      sectionTwo.ExcersiseTwo();
     }
   }
 }
