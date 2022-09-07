64f40a93a9aa308c104a9554a9193e6420e01dce
hdzc21 <hadzic.nenin@gmail.com>
2022-09-07 10:32:31 +0930

    Complete section 4 excersise

--- Intermediate/IntermediateExcersises/IntermediateExcersises/Main.cs
+++ Intermediate/IntermediateExcersises/IntermediateExcersises/Main.cs
@@ -4,10 +4,14 @@
   {
     static void Main(string[] args)
     {
-      // Section 2 Soultions
+      // Section 2 Solutions
       Section2 sectionTwo = new Section2();
       //sectionTwo.ExcersiseOne();
-      sectionTwo.ExcersiseTwo();
+      //sectionTwo.ExcersiseTwo();
+
+      // Section 4 Solutions
+      Section4 sectionFour = new Section4();
+      sectionFour.ExceriseOne();
     }
   }
 }
